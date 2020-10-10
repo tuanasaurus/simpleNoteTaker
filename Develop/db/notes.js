7fd72120bb1b7db43e0cfe9d3fbe759357b51c2d
Tuan Phan <tuan@tuan.design>
2020-10-09 17:58:38 -0700

    wrote out the notes.js to start off the app

---
+++ Develop/db/notes.js
@@ -0,0 +1,53 @@
+const util = require('util');
+const fs = require('fs');
+
+// until.promisify taking a function following the common error
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Creating the class notes constructor and GET notes
+
+class Notes {
+    constructor () {
+        this.idDum = 0;
+    }
+    read () {
+        return readFileAsync("db/db.json", "utf8");
+    }
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
+    getNotes () {
+        console.logs("get notes")
+        return this.read().then(notes => {
+            console.log(notes)
+            let notesArray;
+            try {
+             notesArray = [].concat(JSON.parse(notes));
+            }
+            catch (err) {
+                notesArray = [];
+            }
+            return notesArray;
+            })
+    }
+    // Adding notes with title and text
+    addNotes(note) {
+        console.log("add notes");
+        const { title, text } = note;
+        const newNote = { title, text, id: ++this.idDum }
+        return this.getNotes()
+            .then(notes => [...notes, newNote])
+            .then(updateNotes => this.write(updateNotes))
+            .then(() => newNote)
+    }
+    // Remove notes
+    removeNote(id) {
+        console.log("remove note");
+        return this.getNotes()
+            .then(notes => notes.filter(note =>.id !== parseInt(id)))
+            .then(updatedNotes => this.write(updatedNotes))
+    }
+}
+
+module.exports = new Notes();
